818770efa4357da12ff2fffd08157bdf4cded6be
huy1108 <quochuy11081999@gmail.com>
2022-03-16 23:00:37 +0700

    a

--- 1.js
+++ 1.js
@@ -1,8 +1,1 @@
-function groupByLength(xs, key) {
-    return xs.reduce(function (rv, x) {
-        (rv[x[key]] = rv[x[key]] || []).push(x);
-        return rv;
-    }, {});
-};
-
-console.log(groupByLength(['one', 'two', 'three', 'four', 'five'], 'length'));
+const test = 'new-branch'
